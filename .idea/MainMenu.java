c07843843b8afa41e58e2fb1342ae8602a72ce80
Ryan Hall <ryan.hall@mullenmail.com>
2026-03-24 17:05:09 -0600

    Behold, Stuff!

--- .idea/MainMenu.java
+++ .idea/MainMenu.java
@@ -1,3 +1,17 @@
-public class MainMenu {
 
+
+
+public class MainMenu
+{
+    public static void main(String[] args)
+    {
+        CreateMenu();
+    }
+    public void CreateMenu()
+    {
+        System.out.println("What do you want to do?");
+        System.out.println("Add or Delete Student");
+        System.out.println("Change Student Grades/Schedules");
+        System.out.println("Sort Students")
+    }
 }
--- .idea/Students.java
+++ .idea/Students.java
@@ -1,9 +1,11 @@
 class Students {
     int id;
     String grade;
+    double gpa
 
-    public Students(int id, String grade) {
+    public Students(int id, String grade, double gpa) {
         this.id;
         this.grade;
+        this.gpa;
     }
 }
