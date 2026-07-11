09013af94ab88de28fefc66ef7defbc89bbe7cac
Ryan Hall <ryan.hall@mullenmail.com>
2026-07-11 13:04:59 -0600

    Grades added

--- .idea/Challenge 1/Analyzer.java
+++ .idea/Challenge 1/Analyzer.java
@@ -5,6 +5,19 @@ import java.util.ArrayList;
 public class Analyzer
 {
     static ArrayList<Integer> grade = new ArrayList<>();
+
+    public static ArrayList<Integer> getGrade() {
+        grade.add(85);
+        grade.add(92);
+        grade.add(47);
+        grade.add(63);
+        grade.add(78);
+        grade.add(91);
+        grade.add(55);
+        grade.add(100);
+        return grade;
+    }
+
     public static void main(String[] args) {analyze();}
     public static void analyze()
     {
